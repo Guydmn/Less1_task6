6357733a4636dc496ef1a31a58fde045ea9c2671
Roman Guydmn <romanguydmn@gmail.com>
2023-01-20 17:54:38 +0400

    Задача 6

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+
+if (b == 0)
+Console.Write($"Число {a} четное");
+else 
+Console.Write($"Число {a} нечетное");
